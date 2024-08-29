433ff5e7c745b22189bc8274defb4230d3041c9e
Brayan Ocampo <mrx.source.mrx@gmail.com>
2024-08-29 07:58:35 -0500

    change path of rofi theme

--- .config/i3/rofi/run.sh
+++ .config/i3/rofi/run.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-rofi -no-config -no-lazy-grab -show run -modi run -theme ~/.local/share/rofi/themes/sonokai/sonokai.rasi
+rofi -no-config -no-lazy-grab -show run -modi run -theme ~/.config/i3/rofi/sonokai/sonokai.rasi
--- .config/i3/rofi/window.sh
+++ .config/i3/rofi/window.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
-
-rofi -no-config -no-lazy-grab -show window -modi window -theme ~/.local/share/rofi/themes/sonokai/sonokai.rasi
+rofi -no-config -no-lazy-grab -show window -modi window -theme ~/.config/i3/rofi/sonokai/sonokai.rasi
